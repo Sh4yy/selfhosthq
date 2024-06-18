0d05818322d57a626160d68d54ec5c1ce9845723
Shayan <ltaslim@montgomerycollege.edu>
2024-06-18 01:44:08 -0400

    fixed env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
+
+declare namespace App {
+  interface Locals extends Runtime {}
+}
