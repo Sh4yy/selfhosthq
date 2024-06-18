465ac1e5a100c9b98d7d465ac58df854c507b843
wadder12 <apwade75009@gmail.com>
2024-06-18 16:43:58 -0500

    updated config file for tailwind and added moblie - should be OK hopefully

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,19 +2,69 @@ import colors from 'tailwindcss/colors'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: 'class',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		fontFamily: {
-			mono: ['JetBrains Mono', 'monospace'],
-		},
-		extend: {
-			colors: {
-				gray: colors.neutral,
-			},
-		},
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
+  darkMode: 'class',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    fontFamily: {
+      mono: ['JetBrains Mono', 'monospace'],
+    },
+    extend: {
+      colors: {
+        gray: colors.neutral,
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.gray.100'),
+            a: {
+              color: theme('colors.blue.500'),
+              '&:hover': {
+                color: theme('colors.blue.700'),
+              },
+            },
+            h1: {
+              fontSize: theme('fontSize.2xl'),
+              color: theme('colors.gray.100'),
+              '@screen sm': {
+                fontSize: theme('fontSize.3xl'),
+              },
+              '@screen lg': {
+                fontSize: theme('fontSize.4xl'),
+              },
+            },
+            h2: {
+              color: theme('colors.gray.100'),
+            },
+            p: {
+              fontSize: theme('fontSize.sm'),
+              color: theme('colors.gray.100'),
+              '@screen sm': {
+                fontSize: theme('fontSize.base'),
+              },
+              '@screen lg': {
+                fontSize: theme('fontSize.lg'),
+              },
+            },
+            code: {
+              color: theme('colors.gray.100'),
+              backgroundColor: theme('colors.gray.800'),
+            },
+            blockquote: {
+              color: theme('colors.gray.100'),
+              borderLeftColor: theme('colors.gray.700'),
+            },
+            'h1, h2, h3, h4, h5, h6': {
+              color: theme('colors.gray.100'),
+            },
+            strong: {
+              color: theme('colors.gray.100'),
+            },
+          },
+        },
+      }),
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
